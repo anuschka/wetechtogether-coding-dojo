2ced7d2d4bf016512dfb5563aefac1e20af48aac
anuschka <sustic@gmail.com>
2024-09-23 22:08:04 +0200

    Added maven dependecies

--- src/test/java/NumberToWordsTest.java
+++ src/test/java/NumberToWordsTest.java
@@ -1,17 +1,19 @@
 import org.example.NumberToWords;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class NumberToWordsTest {
     @Test
     public void convertNumbersToWords() {
-        Assert.assertEquals("zero", NumberToWords.convertNumberToWords(0));
-        Assert.assertEquals("one", NumberToWords.convertNumberToWords(1));
-        Assert.assertEquals("twenty", NumberToWords.convertNumberToWords(20));
-        Assert.assertEquals("thirty five", NumberToWords.convertNumberToWords(35));
-        Assert.assertEquals("four hundred and twenty one", NumberToWords.convertNumberToWords(421));
-        Assert.assertEquals("five hundred and fifty five", NumberToWords.convertNumberToWords(555));
-        Assert.assertEquals("six hundred and seventy four", NumberToWords.convertNumberToWords(674));
+        assertEquals("zero", NumberToWords.convertNumberToWords(0));
+        assertEquals("one", NumberToWords.convertNumberToWords(1));
+        assertEquals("twenty", NumberToWords.convertNumberToWords(20));
+        assertEquals("thirty five", NumberToWords.convertNumberToWords(35));
+        assertEquals("four hundred and twenty one", NumberToWords.convertNumberToWords(421));
+        assertEquals("five hundred and fifty five", NumberToWords.convertNumberToWords(555));
+        assertEquals("six hundred and seventy four", NumberToWords.convertNumberToWords(674));
 
     }
 }
