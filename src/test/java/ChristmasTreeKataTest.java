9170dc5cb6586fdef204840a5fa3411973ab034d
anuschka <sustic@gmail.com>
2024-09-23 22:09:27 +0200

    Added maven dependecies

--- src/test/java/ChristmasTreeKataTest.java
+++ src/test/java/ChristmasTreeKataTest.java
@@ -1,31 +1,32 @@
 import org.example.ChristmasTreeKata;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChristmasTreeKataTest {
     @Test
     public void xMasTree0() {
         List<String> expected = List.of("no tree");
-        Assert.assertEquals(expected, ChristmasTreeKata.drawChristmasTree(0));
+        assertEquals(expected, ChristmasTreeKata.drawChristmasTree(0));
     }
 
     @Test
     public void xMasTree1() {
         List<String> expected = List.of("x");
-        Assert.assertEquals(expected, ChristmasTreeKata.drawChristmasTree(1));
+        assertEquals(expected, ChristmasTreeKata.drawChristmasTree(1));
     }
 
     @Test
     public void xMasTree2() {
         List<String> expected = List.of(" x"," x");
-        Assert.assertEquals(expected, ChristmasTreeKata.drawChristmasTree(2));
+        assertEquals(expected, ChristmasTreeKata.drawChristmasTree(2));
     }
 
     @Test
     public void xMasTree3() {
         List<String> expected = List.of("  x"," xxx","  x");
-        Assert.assertEquals(expected, ChristmasTreeKata.drawChristmasTree(3));
+        assertEquals(expected, ChristmasTreeKata.drawChristmasTree(3));
     }
 }
